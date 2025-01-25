6df0302009c2b6d27d1482e6937cdfe827a94054
SarahJoquebede <j.sarah@escolar.ifrn.edu.br>
2025-01-25 19:35:15 -0300

    Add files via upload
    Getters e Setters adicionados

--- Ingrediente.java
+++ Ingrediente.java
@@ -1,18 +1,37 @@
 package projetinho;
 
 public class Ingrediente {
-    String nome;
     double quantidade;
+    String nome;
     String unidadeDeMedida;
-
-    public Ingrediente(String nome, double quantidade, String unidadeDeMedida) {
-        this.nome = nome;
-        this.quantidade = quantidade;
-        this.unidadeDeMedida = unidadeDeMedida;
+    public Ingrediente(String nome, double quantidade, String unidadeDeMedida){
+        this.nome=nome;
+        this.quantidade=quantidade;
+        this.unidadeDeMedida=unidadeDeMedida;
     }
+    public double getQuantidade(){
+        return quantidade;
+    }
+    public void setQuantidade(double quantidade){
+        this.quantidade=quantidade;
+    }
+    public String getNome(){
+        return nome;
+    }
+    public void setNome(String nome){
+        this.nome=nome;
+    }
+    public void setUnidadeDeMedida(String unidadeDeMedida){
+        this.unidadeDeMedida=unidadeDeMedida;
+    }
+    public String getUnidadeDeMedida(){
+        return unidadeDeMedida;
+    }
+
 
     @Override
-    public String toString() {
-        return nome + " (" + quantidade + " " + unidadeDeMedida + ")";
+    public String toString(){
+        return "Nome:" + nome + " Quantidade: "+ + quantidade+ " " + unidadeDeMedida;
     }
+
 }
--- Receita.java
+++ Receita.java
@@ -1,5 +1,45 @@
 package projetinho.com;
 
+import java.util.List;
+
+import projetinho.Ingrediente;
+
 public class Receita {
+    String modoDePreparo;
+    double porcoes;
+    List<Ingrediente>ingredientes;
+    public Receita(String modoDePreparo, double porcoes, List<Ingrediente>ingredientes){
+        this.modoDePreparo=modoDePreparo;
+        this.porcoes=porcoes;
+        this.ingredientes=ingredientes;
+    }
+    public String getModoDePreparo(){
+        return modoDePreparo;
+    }
+    public void setModoDePreparo(String modoDePreparo){
+        this.modoDePreparo=modoDePreparo;
+    }
+    public double getPorcoes(double porcoes){
+        return porcoes;
+    }
+    public void setPorcoes(double porcoes){
+        this.porcoes=porcoes;
+    }
+    public List<Ingrediente> getIngredientes(){
+        return ingredientes;
+    }
+    public void setIngredientes(List<Ingrediente>novoIngredientes){
+        this.ingredientes=novoIngredientes;
+    }
+    public void addIngrediente(Ingrediente ingrediente){
+        this.ingredientes.add(ingrediente);
+    }
+    public void removeIngredient(Ingrediente ingrediente){
+        this.ingredientes.remove(ingrediente);
+    }
+
+
+
+
 
 }
