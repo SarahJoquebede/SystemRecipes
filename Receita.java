1e4e13085ddaa82c0ab0706a7c81708ad1253b67
SarahJoquebede <j.sarah@escolar.ifrn.edu.br>
2025-01-25 19:32:56 -0300

    Add files via upload
    Adicionei os métodos getters e setters para os atributos

--- Receita.java
+++ Receita.java
@@ -1,26 +1,5 @@
 package projetinho.com;
 
-import java.util.List;
-
-import projetinho.Ingrediente;
-
 public class Receita {
-    String modoDePreparo;
-    double porcoes;
-    List<Ingrediente>ingredientes;
-    public Receita(String modoDePreparo, double porcoes, List<Ingrediente>ingredientes){
-        this.modoDePreparo=modoDePreparo;
-        this.porcoes=porcoes;
-        this.ingredientes=ingredientes;
-    }
-    public void addIngrediente(Ingrediente ingrediente){
-        this.ingredientes.add(ingrediente);
-    }
-    public void removeIngredient(Ingrediente ingrediente){
-        this.ingredientes.remove(ingrediente);
-    }   
-
-
-
 
 }
